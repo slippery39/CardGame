13cca27a9203b968bbac5cb64bcafb470029e757
Shayne <shayne_quinton@hotmail.com>
2022-02-18 15:20:15 -0500

    Moved Card Template to a Prefab, Updated our lane structure in the scene view.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,17 +10,16 @@ public class GameController : MonoBehaviour
     [SerializeField]
     private CardGame _gameState;
     [SerializeField]
-    private Transform _player1LanePosition;
+    private Transform _player1Lanes;
     [SerializeField]
-    private Transform _player2LanePosition;
+    private Transform _player2Lanes;
 
     void Start()
     {
         _gameState = new CardGame();
         //Spawn our cards in some "lanes" for the UI
-
-        FillLaneWithCards(_player1LanePosition, _gameState.Player1Lane);
-        FillLaneWithCards(_player2LanePosition, _gameState.Player2Lane);
+        UpdateLane(_player1Lanes, _gameState.Player1Lane);
+        UpdateLane(_player2Lanes, _gameState.Player2Lane);
     }
 
     private void Update()
@@ -29,9 +28,8 @@ public class GameController : MonoBehaviour
        if (Input.GetKeyDown(KeyCode.B))
         {
             _gameState.BattleSystem.ExecuteBattles(_gameState.Player1Lane, _gameState.Player2Lane);
-            UpdateLane(_player1LanePosition, _gameState.Player1Lane);
-            UpdateLane(_player2LanePosition, _gameState.Player2Lane);
-           
+            UpdateLane(_player1Lanes, _gameState.Player1Lane);
+            UpdateLane(_player2Lanes, _gameState.Player2Lane);           
         }
     }
 
@@ -49,20 +47,6 @@ public class GameController : MonoBehaviour
         for (int i = 0; i < cardsInLane.Count; i++)
         {
             uiCards[i].GetComponent<UICard>().SetFromCardData(cardsInLane[i].CurrentCardData);
-        }
-    }
-
-    private void FillLaneWithCards(Transform laneInScene, List<CardInstance> cardsInLane)
-    {
-        int cardIndex = 0;
-        foreach (var cardInstance in cardsInLane)
-        {
-            var card = CreateCardWithData(cardInstance.CurrentCardData);
-            card.transform.SetParent(laneInScene);
-            var cardXSize = card.GetComponent<UICard>().CardArtRenderer.bounds.size.x;
-            var cardPosition = (cardIndex * (cardXSize + cardXSize/2));
-            card.transform.localPosition = new Vector3(cardPosition, 0, 0);
-            cardIndex++;
         }
     }
     #endregion
