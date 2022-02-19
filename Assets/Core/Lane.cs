8e807095496f5efef7edf3881a62bd6d01506288
Shayne <shayne_quinton@hotmail.com>
2022-02-19 13:59:20 -0500

    fixed null error - c#8.0 requires you to specify if a reference type can be null

--- Assets/Core/Lane.cs
+++ Assets/Core/Lane.cs
@@ -4,19 +4,19 @@
 public class Lane
 {
 
+#nullable enable
     private CardInstance? _unitInLane;
 
     #region Public Properties
-#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
     public CardInstance? UnitInLane { get { return _unitInLane; } set { _unitInLane = value; } }
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+#nullable restore
     public int LaneId { get; set; }
     #endregion
 
     #region Public Methods
 
     public bool IsEmpty()
-    {        
+    {
         return _unitInLane == null;
     }
 
