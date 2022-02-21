06708fd826e2d2439341f472e25ae7a9d1f7a7a1
Shayne <shayne_quinton@hotmail.com>
2022-02-21 16:33:15 -0500

    Added in IZone interface

--- Assets/Core/Lane.cs
+++ Assets/Core/Lane.cs
@@ -2,7 +2,7 @@
 
 //NOTE - Unity does not support null in serialization. If we want things to be serialized, we should use the null object pattern instead.
 //Don't use System.Serializable unless we are 100% sure nothing will be set to null.
-public class Lane
+public class Lane : IZone
 {
     private CardInstance _unitInLane;
     //private bool _isEmpty = true; //fixes the bug where unity thinks there is something in here even when it should be null? not sure why it is doing that.
@@ -10,8 +10,10 @@ public class Lane
     //this breaks the IsEmpty() method as it checks for null.
     //need to find out why that is still. or perhaps just avoid using nulls and use the null object pattern.
     #region Public Properties
-    public CardInstance UnitInLane { get { return _unitInLane; } set { _unitInLane = value;} }
+    public CardInstance UnitInLane { get { return _unitInLane; } set { _unitInLane = value; } }
     public int LaneId { get; set; }
+
+    public string Name => "Lane";
     #endregion
 
     public Lane()
@@ -31,6 +33,23 @@ public class Lane
     {
         //_isEmpty = true;
         _unitInLane = null;
+    }
+
+    public void Add(CardInstance card)
+    {
+        _unitInLane = card;
+    }
+
+    public void Remove(CardInstance card)
+    {
+        if (_unitInLane != card)
+        {
+            Debug.LogError("Trying to remove a card that does not exist in lane");
+        }
+        else
+        {
+            _unitInLane = null;
+        }
     }
     #endregion
 }
---
+++ Assets/Core/Zone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Thoughts here,
+//Our Zone Transition System will need the Add and Remove methods as it will need to be able to move cards between zones without
+//knowing the exact details of the zone.
+
+//Example
+
+//Playing a Creature from Hand
+//ZoneTransitionSystem.ChangeZones(CardInstance card, IZone zone1, IZone zone2);
+
+//orrr
+
+//Move to Lane
+
+//UnitSummonSystem.SummonUnit(CardInstance card,IZone fromZone,Lane laneTo); //this way we could summon creatures from our hand, our discard pile
+//or even some sort of random zone that we make up (The Twilight Zone!).
+//SpellCastingSystem.CastSpell(CardInstance card,IZone fromZone); //needs to move it to the discard pile or whatever.
+//same as above, we could be casting it from our hand, or we could be casting it from some other zone...
+
+public interface IZone
+{
+    string Name { get; }
+    void Add(CardInstance card);
+    //We may not have the actual card reference for whatever reason. 
+    //So search for a UID instead?
+    //Not sure the best way to deal with this.
+    void Remove(CardInstance card);
+}
+
+
+
+
