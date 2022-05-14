738b92eedafaeea333dfb4b164719eb6fcf00d52
Shayne <shayne_quinton@hotmail.com>
2022-05-13 21:40:02 -0400

    Added CanPayManaCost Method

--- Assets/Core/Systems/IManaSystem.cs
+++ Assets/Core/Systems/IManaSystem.cs
@@ -10,6 +10,14 @@ public interface IManaSystem
     bool CanPlayManaCard(CardGame cardGame, Player player, CardInstance card);
     void PlayManaCard(CardGame cardGame, Player player, CardInstance card);
 
+    /// <summary>
+    /// Checks whether a player can theoretically pay a mana cost. Mana Cost is passed in as a string format. 
+    /// </summary>
+    /// <param name="cardGame"></param>
+    /// <param name="player"></param>
+    /// <param name="manaCost"></param>
+    bool CanPayManaCost(CardGame cardGame, Player player, string manaCost);
+
 }
 
 public class DefaultManaSystem : IManaSystem
@@ -52,5 +60,10 @@ public class DefaultManaSystem : IManaSystem
         var manaCard = card.CurrentCardData as ManaCardData;
         AddMana(cardGame, player, Convert.ToInt32(manaCard.ManaAdded));
         cardGame.ZoneChangeSystem.MoveToZone(cardGame, card, player.DiscardPile);        
+    }
+
+    public bool CanPayManaCost(CardGame cardGame, Player player, string manaCost)
+    {
+        return (player.Mana >= Convert.ToInt32(manaCost));
     }
 }
