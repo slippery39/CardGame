68316a1970a3da0e3e896dff64653d706b20fc65
Shayne <shayne_quinton@hotmail.com>
2022-05-28 02:37:38 -0400

    Updated the additional cost rules text

--- Assets/Core/ActivatedAbility.cs
+++ Assets/Core/ActivatedAbility.cs
@@ -1,6 +1,29 @@
 ﻿public class ActivatedAbility : CardAbility
 {
-    public override string RulesText => $@"{ManaCost},{AdditionalCost}:{AbilityEffect.RulesText}";
+    public override string RulesText
+    {
+
+        get
+        {
+            string manaCostStr = "";
+            if (ManaCost != "0")
+            {
+                manaCostStr = ManaCost;
+            }
+
+            string additionalCostStr = "";
+            if (HasAdditionalCost())
+            {
+                additionalCostStr = $@"{AdditionalCost.RulesText}:";
+                if (manaCostStr != "")
+                {
+                    manaCostStr += ", ";
+                }
+            }
+
+            return $@"{manaCostStr}{additionalCostStr}{AbilityEffect.RulesText}";
+        }
+    }
     public string ManaCost { get; set; }
     public AdditionalCost AdditionalCost { get; set; } = null;
     public Effect AbilityEffect { get; set; }
