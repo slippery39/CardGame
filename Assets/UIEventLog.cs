b5495ee4ce3ead509e5033fe9f8be02a1e858cfb
Shayne Quinton <shayne_quinton@hotmail.com>
2022-11-20 02:21:55 -0500

    Added in scrolling event log

--- Assets/UIEventLog.cs
+++ Assets/UIEventLog.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIEventLog : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private ScrollRect scrollRect;
     public void SetLog(IEnumerable<string> messages)
-    {
+    {        
         text.text = string.Join("\n", messages);
+        scrollRect.verticalNormalizedPosition = 0;
+        scrollRect.verticalScrollbar.value = 0;
     }
 }
