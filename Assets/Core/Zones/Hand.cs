5a2126f805f3fc5d19e7aa7dbe36cfec55adf70c
Shayne <shayne_quinton@hotmail.com>
2022-12-13 10:03:31 -0500

    removed generic zone class

--- Assets/Core/Zones/Hand.cs
+++ Assets/Core/Zones/Hand.cs
@@ -18,24 +18,6 @@ public class Hand : Zone
     }
 }
 
-public class GenericZone
-{
-    private List<CardInstance> _cards = new List<CardInstance>();
-
-    private string _name = "";
-
-    public List<CardInstance> Cards { get { return _cards; } }
-    public string Name { get => _name; set => _name = value; }
-
-    public void Add(CardInstance card)
-    {
-        _cards.Add(card);
-    }
-    public void Remove(CardInstance card)
-    {
-        _cards.Remove(card);
-    }
-}
 
 
 
