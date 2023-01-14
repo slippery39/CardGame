23b86efcb77f100179c5d894314d0c1a7bd80d07
Shayne Quinton <shayne_quinton@hotmail.com>
2023-01-14 00:46:53 -0500

    created 3D card for new UI

--- Assets/Hand3D.cs
+++ Assets/Hand3D.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Pool;
 
@@ -30,21 +32,41 @@ public class Hand3D : MonoBehaviour
 
     void InitializeCards()
     {
+        //Look for any already initialized cards
+        var alreadyInitializedCards = this.GetComponentsInChildren<Card3D>(true);
+        _instantiatedCards = alreadyInitializedCards.ToList();
+
         for (var i = 0; i < numberOfCards; i++)
         {
             Card3D card;
             if (_instantiatedCards.Count <= i)
             {
+#if UNITY_EDITOR
+                card = PrefabUtility.InstantiatePrefab(_cardPrefab) as Card3D;
+#else
+
                 card = Instantiate<Card3D>(_cardPrefab);
+
+#endif
+
+
                 _instantiatedCards.Add(card);
+
             }
             else
             {
                 card = _instantiatedCards[i];
+                card.gameObject.SetActive(true);
             }
             card.transform.SetParent(transform, false);
             var bounds = card.GetBounds().size;
-            card.transform.localPosition = new Vector3( (bounds.x+cardOffSet) * (i), card.transform.localPosition.y, card.transform.localPosition.z);
+            card.transform.localPosition = new Vector3((bounds.x + cardOffSet) * (i), card.transform.localPosition.y, card.transform.localPosition.z);
+        }
+
+        //Hide any additional cards
+        for (var i = numberOfCards; i < _instantiatedCards.Count; i++)
+        {
+            _instantiatedCards[i].gameObject.SetActive(false);
         }
     }
 }
