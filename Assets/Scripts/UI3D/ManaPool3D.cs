eb73af2712836ac6b61b0ffe3d566dd3ec2d2cf7
Shayne <shayne_quinton@hotmail.com>
2023-02-03 17:57:11 -0500

    Mana3D UI Element

---
+++ Assets/Scripts/UI3D/ManaPool3D.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ManaPool3D : MonoBehaviour
+{
+    [SerializeField] Mana3D _colorlessMana;
+    [SerializeField] Mana3D _whiteMana;
+    [SerializeField] Mana3D _blueMana;
+    [SerializeField] Mana3D _blackMana;
+    [SerializeField] Mana3D _redMana;
+    [SerializeField] Mana3D _greenMana;
+}
