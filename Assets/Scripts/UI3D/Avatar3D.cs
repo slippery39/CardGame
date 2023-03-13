8cb3aa8689f3724b1501c8cfd8ce8bd4fc2d059d
Shayne <shayne_quinton@hotmail.com>
2023-03-13 01:06:40 -0400

    3D UI - avatars are now clickable and highlightable

--- Assets/Scripts/UI3D/Avatar3D.cs
+++ Assets/Scripts/UI3D/Avatar3D.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Avatar3D : MonoBehaviour
+public class Avatar3D : MonoBehaviour, IHighlightable
 {
+
+    [SerializeField]
+    private SpriteRenderer _highlight;
+
     [SerializeField]
     private TextMeshPro text;
+
+    public void Highlight()
+    {
+        Highlight(Color.green);
+    }
+
+    public void Highlight(Color highlightColor)
+    {
+        _highlight.gameObject.SetActive(true);
+        _highlight.color = highlightColor;
+    }
 
     public void SetLifeTotal(int amount)
     {
         text.text = amount.ToString();
+    }
+
+    public void StopHighlight()
+    {
+        _highlight.gameObject.SetActive(false);
     }
 }
