6a913957ac94b468e7fa3670e7df9ac871d1452e
Shayne <shayne_quinton@hotmail.com>
2023-03-18 15:51:05 -0400

    wip - card viewer popup

--- Assets/StackedSliderTest.cs
+++ Assets/StackedSliderTest.cs
@@ -15,21 +15,41 @@ public class StackedSliderTest : MonoBehaviour
     private GameObject _container;
 
     [SerializeField]
-    private float _scrollAmount = 30f;
-
-    [SerializeField]
     float minX = float.MaxValue;
     [SerializeField]
     float maxX = float.MinValue;
+
+    [SerializeField]
+    float maxXReal;
+
+    [SerializeField]
+    bool _showSlider = true;
+
+
+    [SerializeField]
+    Vector3 cameraMin;
+
+    [SerializeField]
+    Vector3 cameraMax;
+
+    [SerializeField]
+    Vector3 cameraDiff;
+
+    [SerializeField]
+    Bounds _containerBounds;
+
+    [SerializeField]
+    Transform _visibleBoundsObject;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         var camera = this.GetComponent<Camera>();
-        Vector3 cameraMin = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
-        Vector3 cameraMax = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
-        Vector3 cameraDiff = cameraMax - cameraMin;
+        cameraMin = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
+        cameraMax = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
+        cameraDiff = cameraMax - cameraMin;
 
 
         float paddingX = 2.5f;
@@ -53,8 +73,20 @@ public class StackedSliderTest : MonoBehaviour
             if (t.transform.localPosition.x - cameraDiff.x + paddingX > maxX)
             {
                 maxX = t.transform.localPosition.x - cameraDiff.x + paddingX;
+                maxXReal = t.transform.localPosition.x;
             }
         }
+
+        if (maxXReal > cameraDiff.x)
+        {
+            _showSlider = true;
+        }
+        else
+        {
+            _showSlider = false;
+        }
+
+        _containerBounds = CalculateLocalBounds(_container);
     }
 
 
@@ -62,16 +94,47 @@ public class StackedSliderTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We need to find trhe min position out of all the items in the container
-        //as well as the max position out of all the items in the container
-
-        //We will lerp the containers position based off of these values.
-
-
-
-        //TODO - Logic to make it so no scrollbar shows if it doesn't go past the screen.
-        _container.transform.localPosition = new Vector3(Mathf.Lerp(minX, minX-maxX, _slider.value),
+        _slider.gameObject.SetActive(_showSlider);
+        CalculateLocalBounds(_container);
+        /*
+        _container.transform.localPosition = new Vector3(Mathf.Lerp(minX, minX - maxX, _slider.value),
             _container.transform.localPosition.y,
             _container.transform.localPosition.z);
+        */
     }
+
+
+
+    private Bounds CalculateLocalBounds(GameObject gameObject)
+    {
+        Quaternion currentRotation = gameObject.transform.rotation;
+        gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        var renderers = gameObject.GetComponentsInChildren<Renderer>();
+
+        if (renderers.Length  == 0)
+        {
+            return new Bounds(gameObject.transform.position, Vector3.zero);
+        }
+
+        Bounds bounds = new Bounds(gameObject.transform.position, Vector3.zero);
+
+        foreach (Renderer renderer in gameObject.GetComponentsInChildren<Renderer>())
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+
+        Vector3 localCenter = bounds.center - gameObject.transform.position;
+        bounds.center = localCenter;
+
+        _visibleBoundsObject.transform.localPosition = gameObject.transform.localPosition + bounds.center;
+        _visibleBoundsObject.transform.localScale = bounds.size;
+
+
+        gameObject.transform.rotation = currentRotation;
+
+        return bounds;
+    }
+
 }
