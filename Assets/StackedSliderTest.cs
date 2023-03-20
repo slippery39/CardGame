2ae6ecde9ae1b2671760ca8ae0a14218b1bb1e84
Shayne <shayne_quinton@hotmail.com>
2023-03-20 04:05:43 -0400

    code cleanup

--- Assets/StackedSliderTest.cs
+++ Assets/StackedSliderTest.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 
 public class StackedSliderTest : MonoBehaviour
 {
+    [SerializeField]
+    Camera _camera;
 
     [SerializeField]
     private Slider _slider;
@@ -15,68 +17,32 @@ public class StackedSliderTest : MonoBehaviour
     private GameObject _container;
 
     [SerializeField]
-    float minX = float.MaxValue;
-    [SerializeField]
-    float maxX = float.MinValue;
-
-    [SerializeField]
-    float maxXReal;
+    Transform _visibleBoundsObject;
 
     [SerializeField]
     bool _showSlider = true;
 
-
-    [SerializeField]
-    Vector3 cameraMin;
-
-    [SerializeField]
-    Vector3 cameraMax;
-
-    [SerializeField]
-    Vector3 cameraDiff;
-
-    [SerializeField]
+    [SerializeField] //TODO - do we need to store local bounds and world space bounds?
     Bounds _containerBounds;
 
     [SerializeField]
-    Transform _visibleBoundsObject;
+    float _paddingLeft = 0.5f; //only applies when the slider is showing.
 
-    [SerializeField]
-    Camera camera;
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        camera = this.GetComponent<Camera>();
-        cameraMin = camera.ViewportToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
-        cameraMax = camera.ViewportToWorldPoint(new Vector3(1, 1, camera.nearClipPlane));
-        cameraDiff = cameraMax - cameraMin;
-
-
-        float paddingX = 2.5f;
-
-        var gameObjects = _container.GetComponentsInChildren<Transform>();
-
-        minX = float.MaxValue;
-        maxX = float.MinValue;
-
-        _containerBounds = CalculateLocalBounds(_container);
+        _camera = this.GetComponent<Camera>();
     }
 
-
-
-    // Update is called once per frame
     void Update()
     {
-
         //BUG - we need to determine the proper order for all of this to happen so that everything updates in one frame.
-
-        Bounds _containerBounds = CalculateLocalBounds(_container);
+        _containerBounds = CalculateLocalBounds(_container);
         _showSlider = !IsInsideCameraViewport(_containerBounds);
         _slider.gameObject.SetActive(_showSlider);
-        var middlePoint = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, camera.nearClipPlane));
+
+        var middlePoint = _camera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, _camera.nearClipPlane));
+
+        //Bounding box is on the screen.
         if (_showSlider == false)
         {
             _container.transform.position = new Vector3(middlePoint.x - (_containerBounds.center.x), middlePoint.y - (_containerBounds.center.y), _container.transform.position.z);
@@ -84,36 +50,17 @@ public class StackedSliderTest : MonoBehaviour
         //Bounding box is off the screen.
         else
         {
-            var leftPoint = camera.ScreenToWorldPoint(new Vector3(0, 0, camera.nearClipPlane));
-            _container.transform.position = new Vector3(leftPoint.x + 0.5f, middlePoint.y - (_containerBounds.center.y), _container.transform.position.z);
+            var leftPoint = _camera.ScreenToWorldPoint(new Vector3(0, 0, _camera.nearClipPlane));
+            _container.transform.position = new Vector3(leftPoint.x + _paddingLeft, middlePoint.y - (_containerBounds.center.y), _container.transform.position.z);
         }
-
-        //The math to make it left aligned would be, screen 0 + half of the first game objects bounds + padding
-
-
-        /*We need to calculate the size of all the cards
-        We need to calculate if the size of all the cards would be contained within the cameras view
-        
-        IF (isContainedInView)
-            center the container inside the camera
-        ELSE 
-            container should be (left or potentially modified via a setting) aligned based on the camera
-            and the scrollbar should show.
-
-        */
-        /*
-        _container.transform.localPosition = new Vector3(Mathf.Lerp(minX, minX - maxX, _slider.value),
-            _container.transform.localPosition.y,
-            _container.transform.localPosition.z);
-        */
     }
-
+   
 
     private bool IsInsideCameraViewport(Bounds bounds)
     {
         //BUG - bounds.min and bounds.max are in local space, not world space
-        Vector3 minPosition = camera.WorldToViewportPoint(_container.transform.position + bounds.min);
-        Vector3 maxPosition = camera.WorldToViewportPoint(_container.transform.position + bounds.max);
+        Vector3 minPosition = _camera.WorldToViewportPoint(_container.transform.position + bounds.min);
+        Vector3 maxPosition = _camera.WorldToViewportPoint(_container.transform.position + bounds.max);
 
         if (minPosition.x < 0 || maxPosition.x > 1)
         {
@@ -122,8 +69,6 @@ public class StackedSliderTest : MonoBehaviour
 
         return true;
     }
-
-
 
     private Bounds CalculateLocalBounds(GameObject gameObject, bool includeParentTransform = false)
     {
@@ -137,11 +82,9 @@ public class StackedSliderTest : MonoBehaviour
             return new Bounds(gameObject.transform.position, Vector3.zero);
         }
 
-
         //If the include parent transform flag is set, we want to include the parents transform as part of the bounds
         //This may not be what is intended if the parent is for example an empty game object and doesn't actually show anything on screen
         Bounds bounds;
-
         if (includeParentTransform)
         {
             bounds = new Bounds(gameObject.transform.position, Vector3.zero);
